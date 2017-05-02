1828e34bf6806467d4f6c322208dfaeccb86ca03
fabiogsantos <fabiogsantos@gmail.com>
2017-05-02 00:07:30 -0300

    Ajustes finais com base na Aula 2

--- js/action_jquery.js
+++ js/action_jquery.js
@@ -2,33 +2,21 @@ $(document).ready(function() {
   
   /*alert("ola mundo");*/
   
-  $("#btnContraste").click(
-	
-	function(){
-				("html").toggleClass("contraste");
-              }	  
+  // Focus no primeiro link 
+  $("a:first").focus();
+    
+  $("#btnContraste").click(function(event)
+    {
+      event.preventDefault(); // pára a ação padrão de um elemento de acontecer.
+	  $("body").toggleClass("contraste");
+    }	  
   )
   
   $(".readmoreComJQuery").hide();
   
-  $(.js-texto a").click(
-	
-	function(){
-				$(".readmoreComJQuery").slidetoogle("slow", 
-													function() {
-																	alert("pronto!");
-															   }
- 												   );
-			  }
-   )  
+  $(".js-texto a").click(function(event){	          
+	event.preventDefault(); // pára a ação padrão de um elemento de acontecer.
+	$(this).parents(".js-texto").find(".readmoreComJQuery").slideToggle("slow");
+  })  
    
-  $("#btnAumentaFonte").click(
-  
-    function(){
-		
-				
-		
-	          }
-   
-   )
-}   
+})   
